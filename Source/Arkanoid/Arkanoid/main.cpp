c495a74f3f2acee63179837efa42d5df34c1b967
Aaron Whitesell <axwhitesell@gmail.com>
2015-05-02 20:03:35 +0300

    Part 1 - The main loop.

---
+++ Source/Arkanoid/Arkanoid/main.cpp
@@ -0,0 +1,39 @@
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+
+
+int main()
+{
+	const unsigned int windowWidth{800}, windowHeight{600};
+	sf::RenderWindow window({windowWidth, windowHeight}, "Arkanoid");
+	window.setFramerateLimit(60);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+
+				case sf::Event::KeyPressed:
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+						window.close();
+					break;
+			}
+		}
+
+		// "Clear" the window from previouly drawn graphics
+		window.clear(sf::Color::Black);
+
+		// Show the window contents
+		window.display();
+	}
+
+	return 0;
+}
