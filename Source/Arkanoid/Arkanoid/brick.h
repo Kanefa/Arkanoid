365737037cb345b910ccaf61f16514bd876f49bc
Aaron Whitesell <axwhitesell@gmail.com>
2015-05-03 23:35:31 +0300

    Part 7 - Draw the bricks.

---
+++ Source/Arkanoid/Arkanoid/brick.h
@@ -0,0 +1,23 @@
+#ifndef BRICK_H
+#define BRICK_H
+
+#include <SFML/Graphics/RectangleShape.hpp>
+
+struct Brick
+{
+			Brick(float x, float y);
+			Brick(const Brick &) = default;
+	Brick &	operator=(const Brick &) = delete;
+
+	float	x() const;
+	float	y() const;
+	float	left() const;
+	float	right() const;
+	float	top() const;
+	float	bottom() const;
+
+	sf::RectangleShape mShape;
+	bool			   mDestroyed; // Has the brick been struck?
+};
+
+#endif
--- Source/Arkanoid/Arkanoid/main.cpp
+++ Source/Arkanoid/Arkanoid/main.cpp
@@ -1,15 +1,19 @@
 #include "ball.h"
+#include "brick.h"
 #include "paddle.h"
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <vector>
 
 namespace global
 {
 	extern const unsigned int windowWidth{800}, windowHeight{600};
 	extern const float ballVelocity{8.f};
 	extern const float paddleWidth{60.f}, paddleHeight{20.f}, paddleVelocity{6.f};
+	extern const float brickWidth{60.f}, brickHeight{20.f};
+	extern const int countBlocksX{11}, countBlocksY{4};
 }
 
 // Define a generic function to check if two shapes are intersecting
@@ -49,6 +53,18 @@ int main()
 	Ball ball{global::windowWidth / 2, global::windowHeight / 2};
 	Paddle paddle{global::windowWidth / 2, global::windowHeight - 50.f};
 
+	const int buffer = 3;
+	const int margin = 22;
+	std::vector<Brick> bricks;
+	for (int column{0}; column < global::countBlocksX; ++column)
+	{
+		for (int row{0}; row < global::countBlocksY; ++row)
+		{
+			bricks.emplace_back((column + 1) * (global::brickWidth + buffer) + margin,
+								(row + 2) * (global::brickHeight + buffer));
+		}
+	}
+
 	sf::RenderWindow window({global::windowWidth, global::windowHeight}, "Arkanoid");
 	window.setFramerateLimit(60);
 
@@ -79,6 +95,11 @@ int main()
 
 		window.draw(ball.mShape);
 		window.draw(paddle.mShape);
+		for (const auto &brick : bricks)
+		{
+			window.draw(brick.mShape);
+		}
+
 		window.display();
 	}
 
